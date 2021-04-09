e9a3e7631ad1c7d4bd06d00b060adf69df3c98e2
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-04-09 18:38:01 +0100

    fix: return proper error code

--- src/middlewares.js
+++ src/middlewares.js
@@ -7,7 +7,7 @@ function notFound(req, res, next) {
 /* eslint-disable no-unused-vars */
 function errorHandler(err, req, res, next) {
   /* eslint-enable no-unused-vars */
-  const statusCode = res.statusCode !== 200 ? res.statusCode : 500;
+  const statusCode = err.statusCode !== 200 ? err.statusCode : 500;
   res.status(statusCode);
   res.json({
     message: err.message,
