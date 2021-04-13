56355d15b728adf59a5f16064a6f9ca27287c68c
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-04-13 14:36:19 +0100

    Name and version showing at startup

--- src/index.js
+++ src/index.js
@@ -4,12 +4,14 @@ const httpsServer = require('./app');
 const port = process.env.PORT || 8400;
 const host = process.env.HOST || 'localhost';
 
-// Script version
+// Service name and version
+process.env.NAME = 'EZ-Cloud Onboarding for Legacy SIEM';
 process.env.VERSION = '0.0.1';
 
 // app.listen(port, host, () => {
 httpsServer.listen(port, host, () => {
   /* eslint-disable no-console */
+  console.log(`${process.env.NAME} - Version: ${process.env.VERSION}`);
   console.log(`Listening: http://${host}:${port}`);
   /* eslint-enable no-console */
 });
