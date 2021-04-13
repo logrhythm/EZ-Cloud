b491d43a4d7d40fbef3aaa400142db3d40266d3e
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-04-13 12:25:56 +0100

    Claen up code

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const morgan = require('morgan');
 const helmet = require('helmet');
 const cors = require('cors');
+const path = require('path');
 
 require('dotenv').config();
 
@@ -11,7 +12,8 @@ const api = require('./api');
 const app = express();
 
 app.use(morgan('dev'));
-app.use(helmet({ contentSecurityPolicy: false })); // Getting rid of contentSecurityPolicy for now, as it forces it to use HTTPS (which we don't have)
+// Getting rid of contentSecurityPolicy for now, as it forces it to use HTTPS (which we don't have)
+app.use(helmet({ contentSecurityPolicy: false }));
 app.use(cors());
 app.use(express.json());
 
@@ -26,11 +28,11 @@ app.use('/api/v1', api);
 // Static web site hosting:
 // - First, the home page
 app.get('/', (req, res) => {
-  res.sendFile(__dirname + '/public_web_root/index.html');
+  res.sendFile(path.join(__dirname, 'public_web_root', 'index.html'));
 });
 // - Second, all the other files/pages
 app.get('/:file(*)', (req, res) => {
-  res.sendFile(__dirname + '/public_web_root/' + req.params.file);
+  res.sendFile(path.join(__dirname, 'public_web_root', req.params.file));
 });
 
 app.use(middlewares.notFound);
