8607114dc8608f8cded015d4507ef06cc1510fe8
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-04-29 13:45:53 +0100

    I18n

--- frontend/src/i18n/en-us/index.js
+++ frontend/src/i18n/en-us/index.js
@@ -4,5 +4,10 @@
 export default {
   failed: 'Action failed',
   success: 'Action was successful',
-  'Save settings to local web browser.': 'Save settings to local web browser.'
+  'Save settings to local web browser.': 'Save settings to local web browser.',
+  'New Pipeline Details': 'New Pipeline Details',
+  'Add new Pipeline': 'Add new Pipeline',
+  Cancel: 'Cancel',
+  'Open this Pipeline': 'Open this Pipeline',
+  'Delete Pipeline': 'Delete Pipeline'
 }
