3c124a37a1acbc11326d9681a94843f1729482f0
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-05-12 16:50:13 +0100

    Backend - Express - Allow up to 50 MB JSON upload

--- backend/src/app.js
+++ backend/src/app.js
@@ -32,7 +32,7 @@ app.use(helmet());
 //   app.use(cors());
 // }
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
 
 const middlewares = require('./middlewares');
 const api = require('./api');
