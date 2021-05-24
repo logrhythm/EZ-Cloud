3c6ab85c676f09e07b907db0fbabe486f5116ddd
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-05-24 18:04:22 +0100

    Backend - Check for new versions on startup

--- backend/src/index.js
+++ backend/src/index.js
@@ -5,11 +5,15 @@
  * @author Tony Massé
  *
  * Created at     : 2021-04-07 15:00:00
- * Last modified  : 2021-04-23 18:06:24
+ * Last modified  : 2021-05-24 17:57:02
  */
+
+// import AutoGitUpdate from 'auto-git-update';
+const AutoGitUpdate = require('auto-git-update');
 
 // eslint-disable-next-line import/no-extraneous-dependencies
 const { EventLogger } = require('node-windows');
+const path = require('path');
 
 const log = new EventLogger('EZ-Cloud Server');
 
@@ -41,6 +45,32 @@ process.env.VERSION = process.env.npm_package_version;
 // eslint-disable-next-line no-console
 console.log(`${process.env.NAME} - Version: ${process.env.VERSION} - Started`);
 log.info(`${process.env.NAME} - Version: ${process.env.VERSION} - Started`);
+
+async function checkForNewVersion () {
+  const updater = new AutoGitUpdate({
+    repository: 'https://github.com/TonyMasse/EZ-Cloud',
+    tempLocation: path.join(process.env.TEMP, 'EZ_Cloud-Upgrade'),
+    ignoreFiles: [
+      'config/database.json',
+      'config/https.cert.pem',
+      'config/https.key.pem',
+      'config/https.keytmp.pem',
+      'config/ssh.json'
+    ],
+    branch: 'master/backend',
+    // executeOnComplete: 'NET RESTART',
+    exitOnComplete: true
+  });
+
+  // eslint-disable-next-line no-console
+  console.log('Checking for updates...');
+  // updater.autoUpdate();
+  const versionComparison = await updater.compareVersions();
+  // eslint-disable-next-line no-console
+  console.log(versionComparison);
+}
+
+checkForNewVersion();
 
 // app.listen(port, host, () => {
 httpsServer.listen(port, host, () => {
