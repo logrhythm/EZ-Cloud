afd41c0a55c193e86e95568a1fc0d832ec5697f6
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-07-02 00:51:45 +0100

    Backend - Better logging for the Socket events

--- src/socket/index.js
+++ src/socket/index.js
@@ -4,14 +4,15 @@ const { installShipper } = require('./controllers/installShipper');
 
 function socketConnect(socket) {
   // eslint-disable-next-line no-console
-  console.log(`Socket.io => CONNECTION : ${socket.id} (Authenticated user: ${socket.handshake.auth.user})`); // XXX
+  // console.log(`Socket.io => CONNECTION : ${socket.id} (Authenticated user: ${socket.handshake.auth.user})`); // XXX
+  console.log('SOCKET', `/CONNECTION/_id/${socket.id}`, 200, '-', socket.handshake.auth.user, '-');
 
   // if (socket.connected) {
   //   socket.emit('tail.log', { code: 'OK', log: 'some log' });
   // }
   socket.on('connect', () => {
     // eslint-disable-next-line no-console
-    console.log('Sokect.on(connect)');
+    console.log('SOCKET', `/connect/_id/${socket.id}`, 200, '-', socket.handshake.auth.user, '-');
   });
 
   // -------------------------------
@@ -19,11 +20,15 @@ function socketConnect(socket) {
 
   // A new tail is requested
   socket.on('tail.init', (payload) => {
+    // eslint-disable-next-line no-console
+    console.log('SOCKET', `/tail.init/_id/${socket.id}`, 200, '-', socket.handshake.auth.user, '-');
     tailInit(socket, payload);
   }); // On: tail.init
 
   // Killing an existing tail
   socket.on('tail.kill', (payload) => {
+    // eslint-disable-next-line no-console
+    console.log('SOCKET', `/tail.kill/_id/${socket.id}`, 200, '-', socket.handshake.auth.user, '-');
     tailKill(socket, payload);
   }); // On: tail.showtaillist
 
@@ -32,6 +37,8 @@ function socketConnect(socket) {
 
   // A new Shipper Install is requested
   socket.on('shipper.install', (payload) => {
+    // eslint-disable-next-line no-console
+    console.log('SOCKET', `/shipper.install/_id/${socket.id}`, 200, '-', socket.handshake.auth.user, '-');
     installShipper(socket, payload);
   }); // On: shipper.install
 }
