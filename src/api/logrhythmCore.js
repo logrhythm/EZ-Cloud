1cc7a5442bab7453e9013bbef700bfdf3e0e7642
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-07-21 11:07:11 +0100

    Backend - API - Add `logrhythmCore` path and module

---
+++ src/api/logrhythmCore.js
@@ -0,0 +1,49 @@
+const express = require('express');
+
+const router = express.Router();
+// Get SQL config
+const fs = require('fs');
+const path = require('path');
+
+const configSql = JSON.parse(fs.readFileSync(path.join(process.env.baseDirname, 'config', 'database.json'), 'utf8')).config;
+// Create SQL object
+const { Connection, Request, TYPES } = require('tedious');
+
+function waitMilliseconds(delay = 250) {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve();
+    }, delay);
+  });
+}
+
+const maxCheckInterval = 10; // Check once every X seconds max, and/or timeout after X seconds
+
+router.get('/', (req, res) => {
+  res.json({
+    message: 'API - logrhythmCore - All good'
+  });
+});
+
+//        ##     ## ######## #### ##       #### ######## #### ########  ######
+//        ##     ##    ##     ##  ##        ##     ##     ##  ##       ##    ##
+//        ##     ##    ##     ##  ##        ##     ##     ##  ##       ##
+//        ##     ##    ##     ##  ##        ##     ##     ##  ######    ######
+//        ##     ##    ##     ##  ##        ##     ##     ##  ##             ##
+//        ##     ##    ##     ##  ##        ##     ##     ##  ##       ##    ##
+//         #######     ##    #### ######## ####    ##    #### ########  ######
+
+const { getDataFromSql, createSqlVariables } = require('../shared/sqlUtils');
+
+
+//        ######## ##     ## ########   #######  ########  ########  ######
+//        ##        ##   ##  ##     ## ##     ## ##     ##    ##    ##    ##
+//        ##         ## ##   ##     ## ##     ## ##     ##    ##    ##
+//        ######      ###    ########  ##     ## ########     ##     ######
+//        ##         ## ##   ##        ##     ## ##   ##      ##          ##
+//        ##        ##   ##  ##        ##     ## ##    ##     ##    ##    ##
+//        ######## ##     ## ##         #######  ##     ##    ##     ######
+
+module.exports = {
+  logrhythmCore: router
+};
