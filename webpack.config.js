4d1846e8d7e3d311887e472ba9e808d163a9be07
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-09-15 18:14:09 +0100

    Backend - Build packaged version - Compress into a versionned archive

--- webpack.config.js
+++ webpack.config.js
@@ -2,40 +2,42 @@ const CopyPlugin = require('copy-webpack-plugin');
 const WebpackShellPluginNext = require('webpack-shell-plugin-next');
 const path = require('path');
 const PACKAGE = require('./package.json');
-const version = PACKAGE.version;
+
+const { version } = PACKAGE;
 
 module.exports = {
-    entry: {
-        service: './src/index.js'
-    },
-    output: {
-        path: path.join(__dirname, 'dist', `EZ-Cloud.v${version}`, 'bin'),
-        filename: '[name].js'
-    },
-    mode: 'production',
-    target: 'node',
-    plugins: [
-        // Copy all the necessary sample files to the relevant place under dist/
-        new CopyPlugin({
-            patterns: [
-                { from: 'config.dist', to: path.join('..', 'config'), toType: 'dir' },
-                { from: 'config.sample', to: path.join('..', 'config.sample'), toType: 'dir' },
-                { from: 'public_web_root', to: path.join('..', 'public_web_root'), toType: 'dir' },
-                { from: '.env.sample', to: path.join('..', '.env.sample'), toType: 'file' },
-                { from: '.env.dist', to: path.join('..', '.env'), toType: 'file' },
-                { from: 'database', to: path.join('..', 'database'), toType: 'dir' }
-            ],
-        })
-        ,
-        new WebpackShellPluginNext({
-            onBuildExit: {
-                scripts: [
-                    // Create a Zip file for this packaged version
-                    'PowerShell.exe -Command Compress-Archive -Path "' + path.join(__dirname, 'dist', `EZ-Cloud.v${version}`) + '" -DestinationPath "' + path.join(__dirname, 'dist', `EZ-Cloud_v${version}.zip` + '" -Force')
-                ],
-                blocking: false,
-                parallel: false
-            }
-        })
-    ]
-}
+  entry: {
+    service: './src/index.js'
+  },
+  output: {
+    path: path.join(__dirname, 'dist', `EZ-Cloud.v${version}`, 'bin'),
+    filename: '[name].js'
+  },
+  mode: 'production',
+  target: 'node',
+  plugins: [
+    // Copy all the necessary sample files to the relevant place under dist/
+    new CopyPlugin({
+      patterns: [
+        { from: 'config.dist', to: path.join('..', 'config'), toType: 'dir' },
+        { from: 'config.sample', to: path.join('..', 'config.sample'), toType: 'dir' },
+        { from: 'public_web_root', to: path.join('..', 'public_web_root'), toType: 'dir' },
+        { from: '.env.sample', to: path.join('..', '.env.sample'), toType: 'file' },
+        { from: '.env.dist', to: path.join('..', '.env'), toType: 'file' },
+        { from: 'database', to: path.join('..', 'database'), toType: 'dir' }
+      ]
+    }),
+
+    new WebpackShellPluginNext({
+      onBuildExit: {
+        scripts: [
+          // Create a Zip file for this packaged version
+          // eslint-disable-next-line prefer-template
+          'PowerShell.exe -Command Compress-Archive -Path "' + path.join(__dirname, 'dist', `EZ-Cloud.v${version}`) + '" -DestinationPath "' + path.join(__dirname, 'dist', `EZ-Cloud_v${version}.zip` + '" -Force')
+        ],
+        blocking: false,
+        parallel: false
+      }
+    })
+  ]
+};
