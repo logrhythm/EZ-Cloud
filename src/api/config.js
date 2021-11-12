eb1c533e65587b7fe04b5ed7e5d6010761e7c30b
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2021-11-12 18:29:40 +0000

    Backend - Code clean-up (`config.js`)

--- src/api/config.js
+++ src/api/config.js
@@ -5,20 +5,6 @@ const router = express.Router();
 const fs = require('fs');
 const path = require('path');
 
-const configSql = JSON.parse(fs.readFileSync(path.join(process.env.baseDirname, 'config', 'database.json'), 'utf8')).config;
-// Create SQL object
-const { Connection, Request, TYPES } = require('tedious');
-
-function waitMilliseconds(delay = 250) {
-  return new Promise((resolve) => {
-    setTimeout(() => {
-      resolve();
-    }, delay);
-  });
-}
-
-const maxCheckInterval = 10; // Check once every X seconds max, and/or timeout after X seconds
-
 // For passwords and tokens cyphering
 const secretPlaceholder = '** PLACEHOLDER - PLACEHOLDER - PLACEHOLDER - PLACEHOLDER - PLACEHOLDER **';
 const { aesEncrypt } = require('../shared/crypto');
