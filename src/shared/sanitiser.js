7024080eae7e94a664569f556c7cd476554181a8
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2022-01-12 22:47:23 +0000

    Revert "Backend - Security - Prevent allowing attackers to perform an open redirection."
    This reverts commit dcebfa23c807b93f9e0e263d83e0f6d2c1c2fb10.

--- src/shared/sanitiser.js
+++ src/shared/sanitiser.js
@@ -11,19 +11,6 @@ function getSafeUidFrom(jsonBody) {
   return cleanUid;
 }
 
-// Clean the provided UID, by removing non letters, numbers and dashes
-function makeSafeUidFrom(rawUid) {
-  // Fail safe with an empty UID
-  if (rawUid && rawUid.length) {
-    return '';
-  }
-
-  // Strip all character that would not be valid in a UID
-  const cleanUid = String(rawUid).replace(/[^-a-zA-Z0-9]/g, '');
-  return cleanUid;
-}
-
 module.exports = {
-  getSafeUidFrom,
-  makeSafeUidFrom
+  getSafeUidFrom
 };
