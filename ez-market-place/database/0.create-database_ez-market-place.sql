85ae44c787d04184acdd136b189827fb3d8a3e22
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2022-02-14 00:48:24 +0000

    Market - DB Creation - Add `messages` table, Add `name` column to `pipeline_templates`, Add/Update `statuses` content

--- ez-market-place/database/0.create-database_ez-market-place.sql
+++ ez-market-place/database/0.create-database_ez-market-place.sql
@@ -17,6 +17,28 @@
 CREATE DATABASE IF NOT EXISTS `ez-market-place` /*!40100 DEFAULT CHARACTER SET utf8mb3 */;
 USE `ez-market-place`;
 
+-- Dumping structure for table ez-market-place.messages
+CREATE TABLE IF NOT EXISTS `messages` (
+  `uid` char(40) NOT NULL DEFAULT uuid(),
+  `sent_On` timestamp NOT NULL DEFAULT current_timestamp(),
+  `updated_On` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
+  `recipient_Uid` char(40) DEFAULT NULL,
+  `sender_Uid` char(40) DEFAULT NULL,
+  `status` tinyint(4) unsigned NOT NULL DEFAULT 5,
+  `message` varchar(4096) DEFAULT NULL,
+  `flags` varchar(4096) DEFAULT '[]',
+  PRIMARY KEY (`uid`),
+  KEY `status` (`status`),
+  KEY `sentOn` (`sent_On`) USING BTREE,
+  KEY `recipientUid` (`recipient_Uid`) USING BTREE,
+  KEY `senderUid` (`sender_Uid`) USING BTREE,
+  CONSTRAINT `FK_messages_publishers` FOREIGN KEY (`recipient_Uid`) REFERENCES `publishers` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_messages_publishers_2` FOREIGN KEY (`sender_Uid`) REFERENCES `publishers` (`uid`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_messages_statuses` FOREIGN KEY (`status`) REFERENCES `statuses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
+
+-- Dumping data for table ez-market-place.messages: ~7 rows (approximately)
+
 -- Dumping structure for table ez-market-place.pipeline_templates
 CREATE TABLE IF NOT EXISTS `pipeline_templates` (
   `uid` char(40) NOT NULL,
@@ -24,6 +46,7 @@ CREATE TABLE IF NOT EXISTS `pipeline_templates` (
   `created` timestamp NOT NULL DEFAULT current_timestamp(),
   `modified` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp(),
   `publisher_uid` char(40) DEFAULT NULL,
+  `name` varchar(50) DEFAULT NULL,
   `collection_configuration` longtext DEFAULT NULL,
   `mapping_configuration` longtext DEFAULT NULL,
   `stats` longtext DEFAULT NULL,
@@ -34,9 +57,7 @@ CREATE TABLE IF NOT EXISTS `pipeline_templates` (
   CONSTRAINT `FK_pipeline_templates_statuses` FOREIGN KEY (`status`) REFERENCES `statuses` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table ez-market-place.pipeline_templates: ~0 rows (approximately)
-/*!40000 ALTER TABLE `pipeline_templates` DISABLE KEYS */;
-/*!40000 ALTER TABLE `pipeline_templates` ENABLE KEYS */;
+-- Dumping data for table ez-market-place.pipeline_templates: ~11 rows (approximately)
 
 -- Dumping structure for table ez-market-place.publishers
 CREATE TABLE IF NOT EXISTS `publishers` (
@@ -46,25 +67,26 @@ CREATE TABLE IF NOT EXISTS `publishers` (
   UNIQUE KEY `display_name` (`display_name`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table ez-market-place.publishers: ~0 rows (approximately)
-/*!40000 ALTER TABLE `publishers` DISABLE KEYS */;
-/*!40000 ALTER TABLE `publishers` ENABLE KEYS */;
+-- Dumping data for table ez-market-place.publishers: ~3 rows (approximately)
 
 -- Dumping structure for table ez-market-place.statuses
 CREATE TABLE IF NOT EXISTS `statuses` (
-  `id` tinyint(3) unsigned NOT NULL,
+  `id` tinyint(4) unsigned NOT NULL,
   `name` varchar(50) NOT NULL DEFAULT '',
   `description` varchar(500) DEFAULT '',
   PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb3;
 
--- Dumping data for table ez-market-place.statuses: ~4 rows (approximately)
+-- Dumping data for table ez-market-place.statuses: ~7 rows (approximately)
 /*!40000 ALTER TABLE `statuses` DISABLE KEYS */;
 INSERT IGNORE INTO `statuses` (`id`, `name`, `description`) VALUES
 	(0, 'Visible', 'The item is publicly visible'),
 	(1, 'Pending review', 'The item is to be reviewed'),
-	(2, 'Hidden', 'The item is hidden'),
-	(3, 'To be deleted', 'The item is flag for deletion');
+	(2, 'Failed Review', 'The item has been reviewed negatively'),
+	(3, 'Hidden', 'The item is hidden'),
+	(4, 'To be deleted', 'The item is flag for deletion'),
+	(5, 'Unread', 'The item is marked as Not Read'),
+	(6, 'Read', 'The item is marked as Read');
 /*!40000 ALTER TABLE `statuses` ENABLE KEYS */;
 
 /*!40101 SET SQL_MODE=IFNULL(@OLD_SQL_MODE, '') */;
