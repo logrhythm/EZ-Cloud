02dd6545aab1e89474e50b87028192ea46370871
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2022-02-23 18:38:37 +0000

    Market Admin - Login page

--- ez-market-place_admin/src/pages/Login/Login.vue
+++ ez-market-place_admin/src/pages/Login/Login.vue
@@ -1,151 +1,29 @@
 <template>
-  <q-page class="flex flex-center">
-    <form>
-      <q-card style="min-width: 350px" :class="(shakyClass ? 'computerSayNo' : '')">
-        <q-card-section>
-          <div class="text-h6">{{ $t('Sign in to EZ Cloud Market Admin') }}</div>
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <q-input
-            dense
-            v-model="username"
-            autofocus
-            :hint="$t('Username')"
-            :rules="[val => !!val || $t('Username is required')]"
-            @keyup.enter="checkCredentials()"
-          />
-        </q-card-section>
-
-        <q-card-section class="q-pt-none">
-          <q-input dense v-model="password" type="password" :hint="$t('Password')" @keyup.enter="checkCredentials()" />
-        </q-card-section>
-
-        <q-card-actions align="stretch">
-          <q-toggle
-            v-model="darkMode"
-            checked-icon="dark_mode"
-            unchecked-icon="light_mode"
-            color="grey"
-            keep-color
-            class="q-my-sm"
-          >
-            <q-tooltip content-style="font-size: 1em">
-              {{ $t('Switch between Light and Dark mode') }}
-            </q-tooltip>
-          </q-toggle>
-            <!-- size="4rem" -->
-          <dir class="col text-bold text-negative fadeOut" v-show="lastAttemptFailed">
-            {{ $t('Authentication failed.') }}
-          </dir>
-          <q-space />
-          <q-btn flat class="q-my-sm" :label="$t('Cancel')" color="" to="/" />
-          <q-btn flat class="q-my-sm" :label="$t('Login')" color="primary" @click="checkCredentials()" :loading="waitingOnServer" />
-        </q-card-actions>
-      </q-card>
-    </form>
+  <q-page class="flex flex-center column q-gutter-lg">
+      <div class="text-h2" style="opacity:.4">
+        Logging you in
+      </div>
+      <div class="text-h6" style="opacity:.4">
+        <q-spinner class="q-mr-sm" />
+        Redirecting you to the multi-factor authentication page.
+      </div>
+      <q-btn label="Cancel" to="/" />
   </q-page>
 </template>
 
 <script>
-import { mapState, mapActions } from 'vuex'
-import mixinSharedDarkMode from 'src/mixins/mixin-Shared-DarkMode'
 
 export default {
   name: 'PageLogin',
   mixins: [
-    mixinSharedDarkMode // Shared computed to access and update the DarkMode
   ],
-  data () {
-    return {
-      username: '',
-      password: '',
-      waitingOnServer: false,
-      lastAttemptFailed: false,
-      shakyClass: false,
-      lastAttemptFailedTimer: null,
-      shakyClassTime: null
-    }
-  }, // data
-  computed: {
-    ...mapState('mainStore', ['jwtToken'])
-  }, // computed
   methods: {
-    ...mapActions('mainStore', ['signIn', 'signOut']),
-    checkCredentials () {
-      if (this.lastAttemptFailedTimer) {
-        clearTimeout(this.lastAttemptFailedTimer)
-      }
-      if (this.shakyClassTime) {
-        clearTimeout(this.shakyClassTime)
-      }
-      this.shakyClass = false
-      this.lastAttemptFailed = false
-      this.signIn({
-        loadingVariableName: 'waitingOnServer',
-        caller: this,
-        apiCallParams: {
-          username: this.username,
-          password: this.password
-        },
-        onSuccessCallBack: this.checkTokenAndMoveOn,
-        onErrorCallBack: this.checkTokenAndMoveOn,
-        debug: false
-      })
-    },
-    checkTokenAndMoveOn () {
-      if (this.jwtToken && this.jwtToken.length) {
-        // And move to Welcome page
-        this.$router.push('/Welcome')
-      } else {
-        this.lastAttemptFailed = true
-        this.shakyClass = true
-        this.shakyClassTimer = setTimeout(() => {
-          this.shakyClass = false
-        }, 1000)
-        this.lastAttemptFailedTimer = setTimeout(() => {
-          this.lastAttemptFailed = false
-        }, 4800)
-      }
+    async login () {
+      this.$auth.signInWithRedirect()
     }
   }, // methods
   mounted () {
-    // First remove any token from previous Login
-    this.signOut()
+    this.login()
   }
 }
 </script>
-
-<style>
-.computerSayNo {
-  position: relative;
-  animation-name: shakeLogin;
-  animation-duration: 5s;
-  animation-iteration-count: infinite;
-  animation-timing-function: cubic-bezier(.36, .07, .19, .97);
-}
-
-@keyframes shakeLogin {
-  0% {left: 0}
-  2% {left: -6px}
-  4% {left: 10px}
-  6% {left: -16px}
-  8% {left: 16px}
-  10% {left: -10px}
-  10% {left: 6px}
-  14% {left: 0}
-}
-
-.fadeOut {
-  animation-name: fadeOut;
-  animation-duration: 5s;
-  animation-iteration-count: infinite;
-}
-
-@keyframes fadeOut {
-   0% {opacity: 1;}
-   40% {opacity: 1;}
-   90% {opacity: 0;}
-   100% {opacity: 0;}
-}
-</style>
