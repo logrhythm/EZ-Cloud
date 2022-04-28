cbe43433d99dc2a3d8adce121e7fdec0a9b2a09f
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2022-04-29 00:29:03 +0100

    Market - Update Test

--- ez-market-place/test/10.api.test.js
+++ ez-market-place/test/10.api.test.js
@@ -8,10 +8,18 @@ process.env.baseDirname = process.env.baseDirname || path.join(__dirname, '..');
 const app = require('../src/app');
 
 const pipelineTemplateUid = uuidv4();
+const dummyPublisherUid = '598ab7fb-e324-45bd-9332-20c0aa9e30ac';
+const dummyPublisherDisplayName = 'Dummy Automated Test Name';
 
-// const nzPublisherHeaderValue = '33f66b74-7cea-4a0e-b981-249c01ae1138:baac1908-8daf-40e3-952f-f0b17c93fe4c';
+// const ezPublisherHeaderValue =
+// '33f66b74-7cea-4a0e-b981-249c01ae1138:baac1908-8daf-40e3-952f-f0b17c93fe4c';
 // 33f66b74-7cea-4a0e-b981-249c01ae1138:baac1908-8daf-40e3-952f-f0b17c93fe4c:2748491
-const encryptedEzPublisherHeaderValue = 'LDiYHGd1w44xVqf17RWncZ3I6IYjfh3R3TVwWerX7j6R6uCKiDipf4W73M/HqOXDZxSaVok6qJWVrXsediAS03N2/WF1G/bAwj5a53qJ349twJ2NkpgMI/sgAE2pIp2plK1dSjN8w2unTa+R37CCK2V1K/+ducTa4dvXWME2hCcduQ8pH2BtHjEoWMZ7OgM7veJ/MnLujFISekLpmNertLYGJUEttazYQhsPEwyr/HzlFgErJgqz+d1nuUh4LXUcYW7kOT1/QUFRsnK+WJD382/hz6gZij5M+kp2GlrQoydhZouZUutVlZwczOXM5L9TlUBrtdoYur8JKXbyu/g0KQ==';
+// eslint-disable-next-line max-len
+// const encryptedEzPublisherHeaderValue = 'LDiYHGd1w44xVqf17RWncZ3I6IYjfh3R3TVwWerX7j6R6uCKiDipf4W73M/HqOXDZxSaVok6qJWVrXsediAS03N2/WF1G/bAwj5a53qJ349twJ2NkpgMI/sgAE2pIp2plK1dSjN8w2unTa+R37CCK2V1K/+ducTa4dvXWME2hCcduQ8pH2BtHjEoWMZ7OgM7veJ/MnLujFISekLpmNertLYGJUEttazYQhsPEwyr/HzlFgErJgqz+d1nuUh4LXUcYW7kOT1/QUFRsnK+WJD382/hz6gZij5M+kp2GlrQoydhZouZUutVlZwczOXM5L9TlUBrtdoYur8JKXbyu/g0KQ==';
+
+// Dummy UIDs
+// 4a261b6e-d0c1-446f-93bd-b0cbfd3cdc8d:598ab7fb-e324-45bd-9332-20c0aa9e30ac:12345
+const encryptedEzPublisherHeaderValue = 'cIsHGg9Xy8IxHsZlv5htfkFNnIiHaJ2bnJhz7W5fhSPl7UM4SOUk8P+cD39VfVkTytyudmul7Y/75nYf2X8ycxKO9feeaJl33QI93p+chj84ro6EAFvA4PTuDmQ62Zdmuufc53smRq5BqLh5ceiIY9V4YqDzB/EMpJuR3l4gnrSGhikrwaFh+QBiQxVHqA4Yfl8uailT91IOFYAYDsyNTlbmX29l93oqGkv8TZcTtDj+vbL6GHcIJxJEMPtjW+Okq+FUtwwO9S86FP6nvX8wxcC8hUy2OghHfzcD+qVoD8YPgZgIwkY3vVF3B1H7b3QwjBPxhoixz+/gqhEiob5r+A==';
 
 function hasNoErrorFieldInBody(res) {
   if ('error' in res.body) throw new Error('endpoint returned an error');
