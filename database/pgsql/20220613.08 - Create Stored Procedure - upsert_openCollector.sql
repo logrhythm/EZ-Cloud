3a0f86367ec1b0a5f5bce4e79538b23edd733579
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2022-06-15 00:17:36 +0100

    Backend - Port EZ to Postgresql - SP `upsert_openCollector`

--- database/pgsql/20220613.08 - Create Stored Procedure - upsert_openCollector.sql
+++ database/pgsql/20220613.08 - Create Stored Procedure - upsert_openCollector.sql
@@ -25,7 +25,7 @@ CREATE PROCEDURE public."upsert_openCollector"
 )
 LANGUAGE 'sql'
 AS $BODY$
-    INSERT INTO public."openCollectors"
+    INSERT INTO public."openCollectors" AS oc
     (
         "uid"
         ,"name"
@@ -58,7 +58,31 @@ AS $BODY$
         ,"fbInstalled" = 1
         ,"fbVersion"
         ,COALESCE("installedShippers", '[]')
-    );
+    )
+    ON CONFLICT ("uid") DO
+        UPDATE SET
+            "name" = "upsert_openCollector"."name"
+            ,"hostname" = "upsert_openCollector"."hostname"
+            ,"port" = "upsert_openCollector"."port"
+            ,"authenticationMethod" = "upsert_openCollector"."authenticationMethod"
+            ,"username" = "upsert_openCollector"."username"
+            ,"password" = CASE
+                WHEN "upsert_openCollector"."password" != '** PLACEHOLDER - PLACEHOLDER - PLACEHOLDER - PLACEHOLDER - PLACEHOLDER **'
+                THEN "upsert_openCollector"."password"
+                ELSE oc."password"
+                END
+            ,"privateKey" = CASE
+                WHEN "upsert_openCollector"."privateKey" != '** PLACEHOLDER - PLACEHOLDER - PLACEHOLDER - PLACEHOLDER - PLACEHOLDER **'
+                THEN "upsert_openCollector"."privateKey"
+                ELSE oc."privateKey"
+                END
+            ,"osVersion" = "upsert_openCollector"."osVersion"
+            ,"ocInstalled" = "upsert_openCollector"."ocInstalled" = 1
+            ,"ocVersion" = "upsert_openCollector"."ocVersion"
+            ,"fbInstalled" = "upsert_openCollector"."fbInstalled" = 1
+            ,"fbVersion" = "upsert_openCollector"."fbVersion"
+            ,"installedShippers" = COALESCE("upsert_openCollector"."installedShippers", '[]')
+    ;
 $BODY$;
 
 ALTER PROCEDURE public."upsert_openCollector"
@@ -70,20 +94,20 @@ GRANT EXECUTE ON PROCEDURE public."upsert_openCollector"
 REVOKE ALL ON PROCEDURE public."upsert_openCollector"
     FROM PUBLIC;
 
-CALL public."upsert_openCollector"(
-	'<IN uid',
-	'<IN name character varying>',
-	'<IN hostname character varying>', 
-	0, -- <IN port integer>, 
-	'<IN "authenticationMethod" character varying>',
-	'<IN username character varying>', 
-	'<IN password character varying>', 
-	'<IN "privateKey" character varying>', 
-	'<IN "osVersion" character varying>',
-	0, -- <IN "ocInstalled" smallint>, 
-	'<IN "ocVersion" character varying>',
-	0, -- <IN "fbInstalled" smallint>, 
-	'<IN "fbVersion" character varying>',
-	'<IN pipelines character varying>', 
-	'<IN "installedShippers" text>'
-)
+-- CALL public."upsert_openCollector"(
+-- 	'<IN uid',
+-- 	'<IN name character varying>',
+-- 	'<IN hostname character varying>', 
+-- 	0, -- <IN port integer>, 
+-- 	'<IN "authenticationMethod" character varying>',
+-- 	'<IN username character varying>', 
+-- 	'<IN password character varying>', 
+-- 	'<IN "privateKey" character varying>', 
+-- 	'<IN "osVersion" character varying>',
+-- 	0, -- <IN "ocInstalled" smallint>, 
+-- 	'<IN "ocVersion" character varying>',
+-- 	0, -- <IN "fbInstalled" smallint>, 
+-- 	'<IN "fbVersion" character varying>',
+-- 	'<IN pipelines character varying>', 
+-- 	'<IN "installedShippers" text>'
+-- )
