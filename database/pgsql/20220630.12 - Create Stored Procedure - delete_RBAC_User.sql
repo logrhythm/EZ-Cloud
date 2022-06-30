9d2d6bf622fa1102f08ea24bd95a02b62d97b33c
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2022-06-30 09:55:57 +0100

    Backend - Port EZ to Postgresql - SP `delete_RBAC_User`

---
+++ database/pgsql/20220630.12 - Create Stored Procedure - delete_RBAC_User.sql
@@ -0,0 +1,54 @@
+-- =============================================
+-- Author:		Tony Massé
+-- Create date: 2022-06-30
+-- Description:	Delete a User in `rbacUserToRole`, based on its ID, and in SQL
+-- =============================================
+
+DROP PROCEDURE IF EXISTS public."delete_RBAC_User";
+
+CREATE PROCEDURE public."delete_RBAC_User"
+(
+    "@userID" integer = NULL -- If none is provided, we just do nothing
+)
+LANGUAGE plpgsql
+AS $BODY$
+DECLARE
+    "@sqlStatement" text;
+    "@userLogin" character varying(500);
+BEGIN
+    IF "@userID" IS NOT NULL
+    THEN
+        -- Get @userLogin for specific @userId
+        SELECT "login"
+            INTO "@userLogin"
+            FROM public."rbacUserToRole"
+            WHERE "id" = "@userID";
+
+        RAISE DEBUG 'userLogin: "%"', "@userLogin"; -- XXXX
+
+        IF "@userLogin" IS NOT NULL 
+        THEN
+            -- Delete record for User in [rbacUserToRole]
+            DELETE FROM public."rbacUserToRole" WHERE "login" = "@userLogin";
+
+            -- Delete the User login
+            IF EXISTS (
+                SELECT FROM pg_catalog.pg_roles WHERE rolname = "@userLogin"
+            )
+            THEN
+                EXECUTE format('
+                    DROP ROLE IF EXISTS %I;
+                    ',
+                    "@userLogin"
+                );
+            END IF;
+        END IF;
+    END IF;
+END
+$BODY$
+
+-- -- Tests
+
+-- -- Assuming the User has ID 3:
+
+-- call public."delete_RBAC_User" (3);
