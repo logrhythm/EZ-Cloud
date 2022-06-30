329f0e64e5877411452ab73896ba71bd3b50f9ca
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2022-06-30 10:47:00 +0100

    Backend - Port EZ to Postgresql - SP `upsert_RBAC_User` - Add Test

--- database/pgsql/20220629.11 - Create Stored Procedure - upsert_RBAC_User.sql
+++ database/pgsql/20220629.11 - Create Stored Procedure - upsert_RBAC_User.sql
@@ -104,3 +104,25 @@ BEGIN
     END IF;
 END;
 $BODY$;
+
+-- =============================================
+-- Tests
+-- =============================================
+
+-- -- Create a new user
+
+-- call public."upsert_RBAC_User" (
+--     NULL,
+--     'EZ_Test_User',
+--     'cb36e823-e68f-46aa-9dc1-71c35cae43b5', -- User role
+--     'P4ssw0rd!!Pa55word!!'
+-- );
+
+-- -- Update the User's RBAC role/group (assuming UserID is 15)
+
+-- call public."upsert_RBAC_User" (
+--     15,
+--     NULL,
+--     'abcde823-e68f-46aa-9dc1-71c35cae43b5', -- New User role
+--     NULL
+-- );
