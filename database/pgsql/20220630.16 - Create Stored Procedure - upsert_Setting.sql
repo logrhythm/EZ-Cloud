e2f086fac49921429984f61c5cd52cef1c5625ba
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2022-06-30 21:55:23 +0100

    Backend - Port EZ to Postgresql - SP `upsert_Setting`

---
+++ database/pgsql/20220630.16 - Create Stored Procedure - upsert_Setting.sql
@@ -0,0 +1,41 @@
+-- =============================================
+-- Author:		Tony Massé
+-- Create date: 2022-06-30
+-- Description:	Upsert Setting record, based on its UID
+-- =============================================
+
+DROP PROCEDURE IF EXISTS public."upsert_Setting";
+
+CREATE PROCEDURE public."upsert_Setting"
+(
+    "@uid" character varying = NULL,
+    "@name" character varying = NULL,
+    "@description" character varying = NULL,
+    "@settingsJson" text = NULL
+)
+LANGUAGE plpgsql
+AS $BODY$
+BEGIN
+    INSERT INTO public."settings" AS s
+    (
+        "uid",
+        "name",
+        "description",
+        "settingsJson"
+    )
+    VALUES
+    (
+        "@uid",
+        "@name",
+        "@description",
+        "@settingsJson"
+    )
+    ON CONFLICT ("uid") DO
+        UPDATE SET
+            "name" = "@name"
+            ,"description" = COALESCE("@description", s."description") -- If provided NULL for these, we re-use the current value
+            ,"settingsJson" = COALESCE("@settingsJson", s."settingsJson") -- If provided NULL for these, we re-use the current value
+    ;
+END
+$BODY$
+
