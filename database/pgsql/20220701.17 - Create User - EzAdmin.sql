4b422604476e4736257056dd075aad73b0754e3b
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2022-07-01 14:50:01 +0100

    Backend - Port EZ to Postgresql - Template to create the EZ Admin User - Fail-secure password
    If one forgets to modify the script, with their intended password, prior to
    running it, it will create the user with a random password.

--- database/pgsql/20220701.17 - Create User - EzAdmin.sql
+++ database/pgsql/20220701.17 - Create User - EzAdmin.sql
@@ -1,6 +1,6 @@
 -- =============================================
 -- Author:		  Tony Massé
--- Create date: 2021-11-11
+-- Create date: 2022-07-01
 -- =============================================
 
 -- Create the EZ Admin User
@@ -9,5 +9,5 @@ call public."upsert_RBAC_User" (
     NULL,
     'ezAdmin',
     'b7972c45-3546-4993-93ab-c0f1d9a0ffae', -- Admin role
-    'CHANGE_ME'
+    gen_random_uuid()::TEXT -- 'CHANGE_ME'
 );
