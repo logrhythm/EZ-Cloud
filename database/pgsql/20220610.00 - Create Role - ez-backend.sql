908b3cb5bef63ad884c7cc583f32e540d356685e
TonyMasse <tony.masse@logrhythm.com>
2022-07-08 14:30:12 +0000

    Backend - Port EZ to Postgresql - `Create Role - ez-backend` - To deal with repeat runs and logging

--- database/pgsql/20220610.00 - Create Role - ez-backend.sql
+++ database/pgsql/20220610.00 - Create Role - ez-backend.sql
@@ -1,13 +1,35 @@
 -- =============================================
 -- Author:		Tony Massé
 -- Create date: 2022-06-10
+-- Modified on: 2022-07-08 - To deal with repeat runs and logging
 -- =============================================
 
-CREATE ROLE "ez-backend" WITH
-  LOGIN
-  NOSUPERUSER
-  INHERIT
-  NOCREATEDB
-  NOCREATEROLE
-  NOREPLICATION;
-  
+DO
+$scooby_doo$
+BEGIN
+  IF EXISTS (
+    SELECT
+      FROM pg_catalog.pg_roles
+    WHERE
+      rolname = 'ez-backend'
+  )
+  THEN
+    RAISE NOTICE '% - Role "ez-backend" already exists. Skipping.', clock_timestamp()::TEXT;
+  ELSE
+    EXECUTE FORMAT(
+      '
+        CREATE ROLE "ez-backend" WITH
+          LOGIN
+          PASSWORD %L
+          NOSUPERUSER
+          INHERIT
+          NOCREATEDB
+          NOCREATEROLE
+          NOREPLICATION;
+      ', gen_random_uuid()::TEXT
+    );
+
+    RAISE NOTICE '% - Role "ez-backend" succesfully created.', clock_timestamp()::TEXT;
+  END IF;
+END
+$scooby_doo$;
