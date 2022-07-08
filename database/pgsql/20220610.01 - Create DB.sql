a1aa8fed0b1f6c96a950abaaed2520135f5f9045
TonyMasse <tony.masse@logrhythm.com>
2022-07-08 21:52:03 +0000

    Backend - Port EZ to Postgresql - Rename the `ez` database to `oc-admin`

--- database/pgsql/20220610.01 - Create DB.sql
+++ database/pgsql/20220610.01 - Create DB.sql
@@ -2,9 +2,10 @@
 -- Author:		Tony Massé
 -- Create date: 2022-06-10
 -- Modified on: 2022-07-08 - To rename `ez-backend` to `oc-admin-backend`
+-- Modified on: 2022-07-08 - To rename the `ez` database to `oc-admin`
 -- =============================================
 
-CREATE DATABASE "ez"
+CREATE DATABASE "oc-admin"
     WITH
     OWNER = postgres
     ENCODING = 'UTF8'
@@ -13,8 +14,8 @@ CREATE DATABASE "ez"
     TABLESPACE = pg_default
     CONNECTION LIMIT = -1;
 
-GRANT ALL ON DATABASE "ez" TO postgres;
+GRANT ALL ON DATABASE "oc-admin" TO postgres;
 
-GRANT TEMPORARY ON DATABASE "ez" TO "oc-admin-backend";
+GRANT TEMPORARY ON DATABASE "oc-admin" TO "oc-admin-backend";
 
-GRANT TEMPORARY, CONNECT ON DATABASE "ez" TO PUBLIC;
+GRANT TEMPORARY, CONNECT ON DATABASE "oc-admin" TO PUBLIC;
