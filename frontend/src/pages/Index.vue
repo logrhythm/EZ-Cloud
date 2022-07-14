db403ad58f3f835b998d3cc152b7d5354eda4da5
TonyMasse <tony.masse@logrhythm.com>
2022-07-14 17:44:26 +0000

    Frontend - Alert user and prompt to fix missing MS SQL config

--- frontend/src/pages/Index.vue
+++ frontend/src/pages/Index.vue
@@ -15,7 +15,38 @@
         </q-tooltip>
         v{{ version }}
       </div>
+      <div>
+        <pre>{{ extraInformation }}</pre>
+      </div>
+      <div>
+        <pre>needToConfigureMsSql: {{ needToConfigureMsSql }}</pre>
+      </div>
     </div>
+
+    <q-dialog v-model="needToConfigureMsSql" persistent>
+      <q-card style="min-width: 350px">
+        <q-card-section class="row items-center">
+          <q-avatar icon="link_off" color="primary" text-color="white" />
+          <div class="text-h6 q-ml-sm">{{ $t('No connection to the SIEM') }}</div>
+        </q-card-section>
+
+        <q-card-section class="q-pt-none">
+          The connection details to the SQL server on the SIEM are missing.
+        </q-card-section>
+
+        <q-card-section class="q-pt-none q-mb-md">
+          Go to the Admin page and fix it.
+        </q-card-section>
+
+        <q-separator />
+
+        <q-card-actions align="right" class="text-primary">
+          <q-btn flat :label="$t('Close')" @click="promptIgnoreConfigureMsSql()" />
+          <q-btn color="primary" :label="$t('Go to Admin')" to="Admin/SIEM/MsSql" />
+        </q-card-actions>
+      </q-card>
+    </q-dialog>
+
   </q-page>
 </template>
 
@@ -27,13 +58,33 @@ export default {
   name: 'PageIndex',
   data () {
     return {
-      version: version
+      version: version,
+      hideNeedToConfigureMsSqlPopup: false
     }
   },
   computed: {
-    ...mapState('mainStore', ['deployment']),
+    ...mapState('mainStore', ['deployment', 'extraInformation']),
     serverVersion () {
       return (this.deployment && this.deployment.version ? this.deployment.version : '?.?.?')
+    },
+    needToConfigureMsSql () {
+      // return this.extraInformation && this.extraInformation.msSqlConnectionConfigMissing
+      return true && !this.hideNeedToConfigureMsSqlPopup
+    }
+  },
+  methods: {
+    promptIgnoreConfigureMsSql () {
+      this.$q.dialog({
+        title: 'Confirm',
+        message: 'A lot of things will NOT work until this is configred. Are you sure you want to ignore this for now?',
+        cancel: 'Yes',
+        ok: 'No',
+        persistent: true
+      }).onOk(() => {
+        // Do nothing
+      }).onCancel(() => {
+        this.hideNeedToConfigureMsSqlPopup = true
+      })
     }
   }
 }
