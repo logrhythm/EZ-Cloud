bf62584fa8e6b17ef7d24e317d65b1bbcf194b6f
TonyMasse <tony.masse@logrhythm.com>
2022-07-26 08:50:11 +0000

    Backend - Use `OC_ADMIN_PASSWORD` environment variable to create the `ocAdmin` user account

--- database/pgsql/20220701.17 - Create User - ocAdmin.sql
+++ database/pgsql/20220701.17 - Create User - ocAdmin.sql
@@ -10,5 +10,5 @@ call public."upsert_RBAC_User" (
     NULL,
     'ocAdmin',
     'b7972c45-3546-4993-93ab-c0f1d9a0ffae', -- Admin role
-    gen_random_uuid()::TEXT -- 'CHANGE_ME'
+    COALESCE(:'OC_ADMIN_PASSWORD', gen_random_uuid()::TEXT) -- 'CHANGE_ME'
 );
