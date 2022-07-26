f6c86de1e02507f35b57ae49627bff3c05bbcccd
TonyMasse <tony.masse@logrhythm.com>
2022-07-26 02:02:51 +0000

    Backend - Starting script for `oc-admin`

---
+++ docker/oc-admin/start_oc_admin.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# =============================================
+# Author:      Tony Massé
+# Create date: 2022-07-08
+# Modified on: 2022-07-26 - To add header and description
+# Description: Pull and Start Production version of the OC-Admin container `oc-admin`.
+# =============================================
+
+echo "### RUN \`oc-admin\` CONTAINER..."
+docker run --publish 8400:8400/tcp --network logrhythm --name oc-admin tonymasse/oc-admin
