d0de81907d77d27f6440f85ee13b412b333dca9d
TonyMasse <tony.masse@logrhythm.com>
2022-08-11 17:46:58 +0000

    Backend - `pseudo_lrctl` - Remove optional `OC_ADMIN_LOGIN` environment variable

--- database/pgsql/reset_ocAdmin_password.sh
+++ database/pgsql/reset_ocAdmin_password.sh
@@ -5,8 +5,6 @@
 # Create date: 2022-08-19
 # Description: Reset the password for Admin User `ocAdmin` to either the new pass in `OC_ADMIN_PASSWORD` environment variable
 #              or a randomly generated password.
-#              Optional: A different login name can be provided in `OC_ADMIN_LOGIN` environment variable. If none, default
-#              `ocAdmin` is used instead.
 #              If `ocAdmin` account doesn't exist anymore, a new one, with the provided password will be created.
 #              If the default `Admin` Role doesn't exist anymore, a new one will be created.
 # =============================================
@@ -15,5 +13,5 @@
 found=$(docker ps --no-trunc | grep "oc-db")
 if [[ ! -z "$found" ]]; then
   # Reset ocAdmin password
-  cat "20220819. - Reset OCAdmin Password.sql" | docker exec -i oc-db psql --username=postgres --dbname oc-admin --set=OC_ADMIN_LOGIN="$OC_ADMIN_LOGIN" --set=OC_ADMIN_PASSWORD="$OC_ADMIN_PASSWORD"
+  cat "20220819. - Reset OCAdmin Password.sql" | docker exec -i oc-db psql --username=postgres --dbname oc-admin --set=OC_ADMIN_PASSWORD="$OC_ADMIN_PASSWORD"
 fi
