25205d3a829d323bddd5292e5dbe07b515da7696
TonyMasse <tony.masse@logrhythm.com>
2022-08-11 17:44:48 +0000

    Backend - `pseudo_lrctl` - Dumb down `20220819. - Reset OCAdmin Password.sql` and use `format()` to mix PgSql and environment variable

--- database/pgsql/20220819. - Reset OCAdmin Password.sql
+++ database/pgsql/20220819. - Reset OCAdmin Password.sql
@@ -1,23 +1,23 @@
 -- =============================================
 -- Author:		  Tony Massé
 -- Create date: 2022-08-19
+-- Modified on: 2022-08-22 - To Dumb down and use "format" to mix PgSql and environment variable
 -- Description: Reset the Password for Role `ocAdmin` with provided new password
 --              provided in `OC_ADMIN_PASSWORD` environment variable.
 --              If no `OC_ADMIN_PASSWORD` is provided, it will use a random password.
---
---              `OC_ADMIN_LOGIN` environment variable can be used to name the Admin
---              account something else than `ocAdmin`. If no `OC_ADMIN_LOGIN` is provided
---              the default login `ocAdmin` is used.
 -- =============================================
 
 -- Reset the Password for the OC Admin User
 
-DO $BODY$
+SELECT format($$
+DO
+$BODY$
 DECLARE
-    "@lowerCaseUserLogin" character varying(500) = lower(COALESCE(:'OC_ADMIN_LOGIN', 'ocAdmin')); -- Try to use `OC_ADMIN_LOGIN` environment variable. Fail that, use default `ocAdmin`
+    "@lowerCaseUserLogin" character varying(500) = lower('ocAdmin'); -- Use default `ocAdmin`
+    "@newPassword" character varying(500) = %L; -- Get `OC_ADMIN_PASSWORD` environment variable as a parameter
     "@userLoginId" integer = NULL;
 BEGIN
-    RAISE DEBUG '"@lowerCaseUserLogin" = %', "@lowerCaseUserLogin";
+    RAISE DEBUG '"@lowerCaseUserLogin" = %%', "@lowerCaseUserLogin";
 
     -- Get the ID of OCAdmin Role
     SELECT rutr."id"
@@ -25,16 +25,16 @@ BEGIN
         FROM public."rbacUserToRole" rutr
         WHERE rutr."login" = "@lowerCaseUserLogin"; -- Must be lowercase
 
-    RAISE DEBUG '"@userLoginId" = %', "@userLoginId";
+    RAISE DEBUG '"@userLoginId" = %%', "@userLoginId";
 
-    RAISE NOTICE 'Reseting Password for User Login "%" (Found as ID "%")', "@lowerCaseUserLogin", "@userLoginId";
+    RAISE NOTICE 'Reseting Password for User Login "%%" (Found as ID "%%")', "@lowerCaseUserLogin", "@userLoginId";
 
     -- Change its password
     call public."upsert_RBAC_User" (
         "@userLoginId",
         "@lowerCaseUserLogin",
         'b7972c45-3546-4993-93ab-c0f1d9a0ffae', -- Admin role
-        COALESCE(:'OC_ADMIN_PASSWORD', gen_random_uuid()::TEXT) -- Try to use `OC_ADMIN_PASSWORD` environment variable. Fail that, generate a random password
+        COALESCE("@newPassword", gen_random_uuid()::TEXT) -- Try to use `OC_ADMIN_PASSWORD` environment variable. Fail that, generate a random password
     );
     RAISE NOTICE 'DONE';
 
@@ -55,3 +55,4 @@ BEGIN
         END IF;
 END
 $BODY$;
+$$, COALESCE(:'OC_ADMIN_PASSWORD', gen_random_uuid()::TEXT))\gexec
