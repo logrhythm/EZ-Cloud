ac87cf7dbb6dd71d8c2bbe4dc139915fa3743d24
TonyMasse <tony.masse@logrhythm.com>
2022-08-11 01:36:03 +0000

    Backend - SQL - PG SQL - Install script - Create User - Comments

--- database/pgsql/20220701.17 - Create User - ocAdmin.sql
+++ database/pgsql/20220701.17 - Create User - ocAdmin.sql
@@ -2,6 +2,7 @@
 -- Author:		  Tony Massé
 -- Create date: 2022-07-01
 -- Modified on: 2022-07-08 - To rename User `ezAdmin` to `ocAdmin`
+-- Modified on: 2022-07-26 - Use `OC_ADMIN_PASSWORD` environment variable to create the `ocAdmin` user account
 -- =============================================
 
 -- Create the OC Admin User
@@ -10,5 +11,5 @@ call public."upsert_RBAC_User" (
     NULL,
     'ocAdmin',
     'b7972c45-3546-4993-93ab-c0f1d9a0ffae', -- Admin role
-    COALESCE(:'OC_ADMIN_PASSWORD', gen_random_uuid()::TEXT) -- 'CHANGE_ME'
+    COALESCE(:'OC_ADMIN_PASSWORD', gen_random_uuid()::TEXT) -- Try to use `OC_ADMIN_PASSWORD` environment variable. Fail that, generate a random password
 );
