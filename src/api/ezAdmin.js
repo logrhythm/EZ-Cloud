284884e21bb0a3c93b3ee5294468a5c0ce14820c
TonyMasse <tony.masse@logrhythm.com>
2022-10-11 09:18:24 +0000

    Backend - API - Admin - New endpoint `UpdateEmdb`

--- src/api/ezAdmin.js
+++ src/api/ezAdmin.js
@@ -1,5 +1,9 @@
 const express = require('express');
 
+// To load the SQL scripts from disk
+const fs = require('fs');
+const path = require('path');
+
 // Load the System Logging functions
 const { logToSystem } = require('../shared/systemLogging');
 
@@ -609,6 +613,156 @@ router.post('/UpdateMsSqlConfig', async (req, res) => {
   }
 
   res.json(updatedMsSqlConfig);
+});
+
+// ##########################################################################################
+// UpdateEmdb
+// ##########################################################################################
+
+router.post('/UpdateEmdb', async (req, res) => {
+  const updatedMsSqlEmdb = {
+    payload: []
+  };
+
+  // Data/fields sent forward:
+  // - username
+  // - password
+
+  // Steps:
+  // - Check params make sense
+  // - Check SQL files are there
+  // - For each file:
+  //   - open the file
+  //   - split in chunks separated by "GO"
+  //   - for each chunk:
+  //     - connect to SQL
+  //     - run the chunk
+  //     - append result to response object
+  //     - bubble up errors to response object
+  // - return response object
+
+  logToSystem('Verbose', `Admin | Attempting to create/update EZ Database on MS SQL | Host: ${req.body.host} | Port: ${req.body.port} | Traffic Is Encrypted: ${(req.body.encrypt === true ? 'Encrypted' : 'Not encrypted')} | user: ${(req.user.username ? req.user.username : '-')}`);
+
+  if (
+    process.env.databaseMode !== 'mssql'
+  ) {
+    // Don't update EZ DB if running on Windows (as it's done by the installer)
+
+    // Get the list of files to run, and their respective target DB
+    const createDatabaseForDocker = JSON.parse(fs.readFileSync(path.join(process.env.baseDirname, 'database', 'create_database_for_Docker.json'), 'utf8'));
+    logToSystem('Debug', `Admin | List of files to run: ${JSON.stringify(createDatabaseForDocker)} | user: ${(req.user.username ? req.user.username : '-')}`);
+
+    // Run each file one by one synchronously
+    if (createDatabaseForDocker && Array.isArray(createDatabaseForDocker)) {
+      // eslint-disable-next-line no-restricted-syntax
+      for await (const sqlScript of createDatabaseForDocker) {
+        if (
+          sqlScript.sourceFilename && sqlScript.sourceFilename.length
+          && sqlScript.targetDatabase && sqlScript.targetDatabase.length
+        ) {
+          logToSystem('Information', `Admin | Running SQL file: "${sqlScript.sourceFilename}" | Against DB: "${sqlScript.targetDatabase}" | user: ${(req.user.username ? req.user.username : '-')}`);
+
+          let sqlFileContent = null;
+          try {
+            // Loading SQL file content, padding it with spaces
+            // The padding is to make sure that "GO" is always surrounded with separators
+            sqlFileContent = ` ${fs.readFileSync(path.join(process.env.baseDirname, 'database', sqlScript.sourceFilename), 'utf8')} `;
+
+            // Split in chunks separated with "GO"
+            const sqlChunks = sqlFileContent.split(/\s[Gg][Oo];?\s/) || [];
+
+            logToSystem('Verbose', `Admin | Number of SQL chunks found: "${sqlChunks.length}" | In SQL file: "${sqlScript.sourceFilename}" | Against DB: "${sqlScript.targetDatabase}" | user: ${(req.user.username ? req.user.username : '-')}`);
+
+            let chunkCounter = 0;
+            // Run each chunk
+            // eslint-disable-next-line no-restricted-syntax
+            for await (const sqlChunk of sqlChunks) {
+              const trimmedSqlChunk = sqlChunk.trim();
+              if (
+                trimmedSqlChunk.length
+              ) {
+                chunkCounter += 1;
+                // Run the chunk of SQL
+                const updatedMsSqlEmdbWithFile = {};
+                await getDataFromMsSql({
+                  msSqlConfig: {
+                    authentication: {
+                      type: 'default',
+                      options: {
+                        userName: req.body.username,
+                        password: req.body.password
+                      }
+                    },
+                    options: {
+                      database: sqlScript.targetDatabase
+                    }
+                  },
+                  targetVariable: updatedMsSqlEmdbWithFile,
+                  returnSingleItem: true,
+                  query: trimmedSqlChunk
+                });
+
+                logToSystem('Debug', `Admin | SQL Chunk run result: "${JSON.stringify(updatedMsSqlEmdbWithFile)}" | user: ${(req.user.username ? req.user.username : '-')}`);
+
+                // Add results to the main Payload
+                updatedMsSqlEmdb.payload.push(
+                  {
+                    sqlFileName: sqlScript.sourceFilename,
+                    sqlChunk: chunkCounter,
+                    ...updatedMsSqlEmdbWithFile
+                  }
+                );
+
+                // Errors management
+                if (
+                  updatedMsSqlEmdbWithFile
+                  && (
+                    updatedMsSqlEmdbWithFile.errors
+                    && Array.isArray(updatedMsSqlEmdbWithFile.errors)
+                    && updatedMsSqlEmdbWithFile.errors.length
+                  )
+                ) {
+                  // Errors
+                  logToSystem('ERROR', `Admin | Failed running file: "${sqlScript.sourceFilename}" | SQL chunk number: "${chunkCounter}" | Errors: "${JSON.stringify(updatedMsSqlEmdbWithFile.errors)}" | user: ${(req.user.username ? req.user.username : '-')}`);
+                  // Make sure we have the `error` structure
+                  if (updatedMsSqlEmdb && !updatedMsSqlEmdb.errors) {
+                    updatedMsSqlEmdb.errors = [];
+                  }
+                  updatedMsSqlEmdbWithFile.errors.forEach((error) => {
+                    updatedMsSqlEmdb.errors.push(error);
+                  });
+
+                  // Prevent any other chunks for the file from running
+                  break;
+                }
+              } else {
+                logToSystem('Verbose', `Admin | Ignoring empty SQL chunks | In SQL file: "${sqlScript.sourceFilename}" | Against DB: "${sqlScript.targetDatabase}" | user: ${(req.user.username ? req.user.username : '-')}`);
+              }
+            }
+          } catch (error) {
+            logToSystem('Error', `Admin | Failed reading file: "${sqlScript.sourceFilename}" | user: ${(req.user.username ? req.user.username : '-')}`);
+          }
+        } else {
+          logToSystem('Warning', `Admin | Ignoring incomplete entry | File: "${sqlScript.sourceFilename}" | DB: "${sqlScript.targetDatabase}" | user: ${(req.user.username ? req.user.username : '-')}`);
+        }
+      }
+    }
+  }
+
+  if (
+    updatedMsSqlEmdb
+    && !(
+      updatedMsSqlEmdb.errors
+      && Array.isArray(updatedMsSqlEmdb.errors)
+      && updatedMsSqlEmdb.errors.length
+    )
+  ) { // 🎉
+    logToSystem('Information', `Admin | EZ Database on MS SQL created/updated | user: ${(req.user.username ? req.user.username : '-')}`);
+  } else { // 😥
+    logToSystem('Warning', `Admin | Failure during creation/update EZ Database on MS SQL | user: ${(req.user.username ? req.user.username : '-')} | Details: ${JSON.stringify(updatedMsSqlEmdb)}`);
+  }
+
+  res.json(updatedMsSqlEmdb);
 });
 
 //        ######## ##     ## ########   #######  ########  ########  ######
