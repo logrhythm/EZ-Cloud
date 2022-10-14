54199e0f2d21ee3dab01d5a56175ef7f1132b14e
TonyMasse <tony.masse@logrhythm.com>
2022-10-14 17:03:01 +0000

    Frontend - Axon style - Login page

--- frontend/src/pages/Login/Login.vue
+++ frontend/src/pages/Login/Login.vue
@@ -1,24 +1,40 @@
 <template>
-  <q-page class="flex flex-center">
+  <q-page class="flex flex-center column" style="background: linear-gradient(279.94deg, #4054B2 4.21%, #007CC2 97.56%);">
+    <div class="q-ma-lg text-white">
+      <svg width="224" height="32" viewBox="0 0 224 32" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M73.7619 3.30609H70.3311V26.0741H82.8691V23.2047H73.7619V3.30609Z" fill="#FCFDFD"/>
+        <path d="M92.0386 8.60822C87.9216 8.60822 85.5513 10.4172 85.5513 13.4737V21.5205C85.5513 24.6394 87.9216 26.386 92.0386 26.386C96.1556 26.386 98.5259 24.577 98.5259 21.5205V13.4737C98.5259 10.3548 96.1556 8.60822 92.0386 8.60822ZM88.7949 14.0975C88.7949 12.3509 89.8554 11.54 92.0386 11.54C94.2218 11.54 95.2823 12.3509 95.2823 14.0975V20.8343C95.2823 22.5809 94.2842 23.3918 92.0386 23.3918C89.793 23.3918 88.7949 22.5809 88.7949 20.8343V14.0975Z" fill="#FCFDFD"/>
+        <path d="M111.501 9.54389C110.128 8.85773 108.756 8.54584 107.072 8.54584C103.828 8.54584 101.895 10.2924 101.895 13.2242V21.0838C101.895 24.078 103.953 25.9493 107.197 25.9493C108.444 25.9493 109.754 25.6375 111.064 25.0761V25.6998C111.064 27.9454 109.567 29.0683 106.573 29.0683C105.138 29.0683 103.204 28.8187 102.456 28.6316L102.144 28.5692V31.4386L102.331 31.501C103.454 31.7505 105.263 31.9377 106.698 31.9377C112 31.9377 114.37 30.0663 114.37 25.8246V8.79535H111.688L111.501 9.54389ZM105.201 13.848C105.201 11.9143 106.635 11.54 107.82 11.54C109.006 11.54 109.879 11.7895 111.127 12.3509V22.2067C110.004 22.7057 108.943 22.9552 107.945 22.9552C105.7 22.9552 105.263 21.5205 105.263 20.2729V13.848H105.201Z" fill="#FCFDFD"/>
+        <path d="M131.649 11.6648V9.66866C131.649 5.42695 129.091 3.36847 123.789 3.36847H118.05V26.0741H121.543V17.5283H124.226L129.653 26.0741H133.582L127.719 17.0293C130.339 16.2808 131.649 14.4718 131.649 11.6648ZM121.543 14.5965V6.23786H123.976C126.908 6.23786 128.218 7.36067 128.218 9.79342V11.2905C128.218 13.5985 126.908 14.5965 123.851 14.5965H121.543Z" fill="#FCFDFD"/>
+        <path d="M143.875 8.60823C142.44 8.60823 140.881 8.9825 139.259 9.79342V3.30609H136.015V26.0741H139.259V12.6628C140.257 12.1638 141.692 11.6024 142.939 11.6024C144.374 11.6024 145.06 12.3509 145.06 13.848V26.0741H148.366V13.0371C148.366 10.1677 146.807 8.60823 143.875 8.60823Z" fill="#FCFDFD"/>
+        <path d="M158.097 21.7076L154.105 8.9201H150.612L156.475 25.8246L156.413 25.9493C155.477 28.694 154.355 29.193 152.857 29.193C152.234 29.193 151.859 29.1306 151.547 29.0683L151.236 28.9435V31.7505L151.423 31.8129C151.859 31.9376 152.358 32 153.232 32C155.914 32 157.972 31.0644 159.532 26.4484L165.395 8.9201H161.84L158.097 21.7076Z" fill="#FCFDFD"/>
+        <path d="M173.692 23.4542C172.07 23.4542 171.384 22.7681 171.384 21.1462V11.6024H175.376V8.85774H171.384V4.55365H168.701L168.078 8.85774V11.6024V21.5829C168.078 24.7018 169.699 26.2612 173.006 26.2612C174.066 26.2612 175.251 26.0741 175.813 25.9494L176 25.887V23.08L175.688 23.2047C175.438 23.3295 174.44 23.4542 173.692 23.4542Z" fill="#FCFDFD"/>
+        <path d="M187.29 8.60823C185.856 8.60823 184.296 8.9825 182.674 9.79342V3.30609H179.431V26.0741H182.674V12.6628C183.672 12.1638 185.107 11.6024 186.355 11.6024C187.789 11.6024 188.475 12.3509 188.475 13.848V26.0741H191.782V13.0371C191.782 10.1677 190.16 8.60823 187.29 8.60823Z" fill="#FCFDFD"/>
+        <path d="M211.68 8.60822C209.871 8.60822 208.062 9.16962 206.627 10.23C205.879 9.16962 204.569 8.60822 202.822 8.60822C201.325 8.60822 199.641 9.10724 198.456 9.85578L198.206 8.79535H195.524V26.0741H198.768V12.538C199.703 11.9143 200.951 11.54 202.136 11.54C203.633 11.54 204.257 12.2885 204.257 13.9727V26.0741H207.501V13.2242C207.501 13.0371 207.501 12.7876 207.438 12.6004C208.623 11.8519 209.746 11.4776 210.869 11.4776C212.304 11.4776 212.99 12.2261 212.99 13.9104V26.0741H216.296V13.2242C216.296 10.2924 214.612 8.60822 211.68 8.60822Z" fill="#FCFDFD"/>
+        <path d="M219.103 9.91815V11.9766H218.479V9.91815H217.73V9.41913H219.851V9.91815H219.103ZM222.534 11.9766V10.1677L221.847 11.9766H221.473L220.787 10.1677V11.9766H220.163V9.41913H221.036L221.66 11.1033L222.284 9.41913H223.095V11.9766H222.534Z" fill="#FCFDFD"/>
+        <path d="M60.4131 0.0623781L59.7269 1.93372C58.3546 5.61404 56.109 11.2281 53.0524 16.9669C47.6879 27.1969 43.0719 29.3801 40.1402 29.3801C36.9589 29.3801 34.9004 26.0117 33.6528 22.5185C32.7795 20.0858 32.4053 17.5283 31.6567 15.0955C30.4715 11.2281 28.9121 8.8577 27.3526 8.8577C23.9842 8.8577 21.9257 14.9084 21.4267 17.2164L21.2396 18.2144H0.904297L1.27857 16.5302C1.34094 16.0936 3.58656 6.05068 9.88675 6.05068C12.9433 6.05068 14.877 9.04483 16.1869 12.4756H13.1928C12.1947 10.1676 11.0096 8.79532 9.88675 8.79532C6.8926 8.79532 5.02126 13.6608 4.39747 15.4698H18.994C19.8049 12.538 22.3 6.05068 27.3526 6.05068C30.1597 6.05068 32.0934 8.67056 33.3409 11.6647C33.5905 12.538 33.9023 13.4113 34.1519 14.2846C35.0252 17.3411 35.6489 20.46 36.9589 23.3918C37.5203 24.6394 38.456 26.5731 40.0778 26.5731C41.8867 26.5731 45.6294 25.1384 50.6197 15.6569C53.1772 10.7914 55.1733 5.9883 56.4209 2.80702H41.8868C40.7639 5.73879 39.0797 9.85575 36.8965 14.2846C36.7094 13.5361 36.5222 12.8499 36.3351 12.1014C36.148 11.4776 35.9608 10.8538 35.7737 10.23C37.4579 6.61209 38.7055 3.30604 39.6411 0.873294L39.953 0H60.4131V0.0623781ZM30.3468 20.3353C26.7289 25.6374 23.9842 26.5731 22.6119 26.5731C21.2396 26.5731 19.7425 24.5146 18.6197 21.1462H15.6879C16.8731 25.076 18.994 29.3177 22.6119 29.3177C24.6704 29.3177 27.6645 28.1949 31.2201 23.7661C30.9706 22.6433 30.6587 21.5205 30.3468 20.3353Z" fill="#FCFDFD"/>
+        </svg>
+    </div>
     <form>
-      <q-card style="min-width: 350px" :class="(shakyClass ? 'computerSaysNo' : '')">
-        <q-card-section>
+      <q-card style="min-width: 350px" class="q-py-md q-px-lg" :class="(shakyClass ? 'computerSaysNo' : '')">
+        <q-card-section class="text-center">
           <div class="text-h6">{{ $t('Sign in to OC Admin') }}</div>
         </q-card-section>
 
         <q-card-section class="q-pt-none">
           <q-input
             dense
+            outlined
             v-model="username"
             autofocus
-            :hint="$t('Username')"
+            :label="$t('Username')"
             :rules="[val => !!val || $t('Username is required')]"
             @keyup.enter="checkCredentials()"
           />
         </q-card-section>
 
         <q-card-section class="q-pt-none">
-          <q-input dense v-model="password" type="password" :hint="$t('Password')" @keyup.enter="checkCredentials()" />
+          <q-input dense v-model="password" outlined type="password" :label="$t('Password')" @keyup.enter="checkCredentials()" />
         </q-card-section>
 
         <q-card-section class="q-py-none">
@@ -27,46 +43,35 @@
           </dir>
         </q-card-section>
 
-        <q-card-actions align="between">
-          <q-toggle
-            v-model="darkMode"
-            checked-icon="dark_mode"
-            unchecked-icon="light_mode"
-            color="grey"
-            keep-color
-            class="q-my-sm"
-          >
-            <q-tooltip content-style="font-size: 1em">
-              {{ $t('Switch between Light and Dark mode') }}
-            </q-tooltip>
-          </q-toggle>
-
-          <q-btn flat icon="translate">
-            <q-tooltip content-style="font-size: 1em">
-              {{ $t('Change language') }}
-            </q-tooltip>
-            <q-menu auto-close anchor="bottom middle" self="top middle">
-              <q-list style="min-width: 10em">
-                <q-item
-                  class="text-center"
-                  :clickable="!language.selected"
-                  v-for="language in languageList"
-                  :key="language.value"
-                  @click="selectLanguage(language.value)"
-                >
-                  <q-item-section :class="(language.selected ? 'text-primary' : '')">
-                    <div>{{ language.nativeLabel }}</div>
-                  </q-item-section>
-                </q-item>
-              </q-list>
-            </q-menu>
-          </q-btn>
-
-          <q-btn flat class="q-my-sm" :label="$t('Login')" color="primary" @click="checkCredentials()" :loading="waitingOnServer" :disable="!canWeLogin" />
+        <q-card-actions>
+          <q-btn class="q-my-sm full-width" :label="$t('Login')" color="primary" @click="checkCredentials()" :loading="waitingOnServer" :disable="!canWeLogin" />
         </q-card-actions>
 
       </q-card>
     </form>
+
+    <div class="q-mt-lg row items-center q-gutter-x-sm text-white">
+      <q-icon name="translate" size="xs"/>
+      <q-select
+        v-model="selectedLanguage"
+        :options="langOptions"
+        emit-value
+        map-options
+        borderless
+        label-color="white"
+        dark
+        @input="(value) => selectLanguage(value)"
+      >
+        <q-tooltip content-style="font-size: 1em">
+          {{ $t('Change language') }}
+        </q-tooltip>
+      </q-select>
+    </div>
+
+    <div class="q-mt-lg text-center text-white">
+      © 2022 LogRhythm, Inc.<br>
+      All Rights Reserved.
+    </div>
 
     <q-page-sticky position="bottom" :offset="[18, 18]">
       <q-fab
@@ -160,7 +165,8 @@ export default {
       ocAdminState: null, // State of OC Admin. Null = status unknown, True = OC Admin is up and responding, False = OC Admin not responding
       loadingPersistenceLayerAvailability: true, // Are we waiting for the API for Persistence Layer Availability
       delayUntilCheck: 0, // How long until we check next API for Persistence Layer Availability
-      persistenceLayerAvailabilityCheckTimer: null
+      persistenceLayerAvailabilityCheckTimer: null,
+      selectedLanguage: this.$i18n.locale
     }
   }, // data
   computed: {
@@ -181,6 +187,26 @@ export default {
         )
       }
       return [{ value: 'en-gb', nativeLabel: 'English' }]
+    },
+    langOptions () {
+      return (
+        languageOptions && Array.isArray(languageOptions)
+          ? languageOptions.reduce(
+            (accumulatedLangages, language) => {
+              accumulatedLangages.push(
+                {
+                  ...language,
+                  label: (language.value !== this.$i18n.locale
+                    ? `${this.$t(language.label)} - ${language.nativeLabel}`
+                    : language.nativeLabel
+                  )
+                }
+              )
+              return accumulatedLangages
+            }, []
+          )
+          : languageOptions
+      )
     },
     pgSqlState () {
       return this.currentPersistenceLayerAvailability.pgSqlAvailable
@@ -247,6 +273,7 @@ export default {
       }
     },
     selectLanguage (selectedLanguage) {
+      console.log(selectedLanguage)
       switchLanguageTo(this, selectedLanguage)
     },
     checkPersistenceLayerAvailability () {
