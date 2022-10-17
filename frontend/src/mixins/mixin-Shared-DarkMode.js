cf160be2d2e8c2b19a8596f762bfbf3bb4e59715
TonyMasse <tony.masse@logrhythm.com>
2022-10-17 00:03:59 +0000

    Frontend - Axon style - Dark/Light mode switch to change Primary colour

--- frontend/src/mixins/mixin-Shared-DarkMode.js
+++ frontend/src/mixins/mixin-Shared-DarkMode.js
@@ -1,4 +1,5 @@
 // mixin-Shared-DarkMode.js
+import { colors } from 'quasar'
 
 export default {
   computed: {
@@ -8,6 +9,14 @@ export default {
       },
       set (value) {
         this.$q.dark.set(value)
+        if (value) {
+          colors.setBrand('primary', 'var(--primaryForDarkMode)') // Only working in Quasar v1
+          colors.setBrand('textForPrimaryButton', 'var(--textForPrimaryButtonForDarkMode)') // Only working in Quasar v1
+          // colors.setCssVar('primary', '__ the value here __') // For when we move to Quasar v2
+        } else {
+          colors.setBrand('primary', 'var(--primaryForLightMode)') // Only working in Quasar v1
+          colors.setBrand('textForPrimaryButton', 'var(--textForPrimaryButtonForLightMode)') // Only working in Quasar v1
+        }
         localStorage.setItem('settings.darkMode', value)
       }
     }
