4875750ebf71e9df1e6109b25c5047076f415295
TonyMasse <tony.masse@logrhythm.com>
2022-10-19 21:38:20 +0000

    Frontend - Axon style - Force CSS variable load at `mount` time

--- frontend/src/mixins/mixin-Shared-DarkMode.js
+++ frontend/src/mixins/mixin-Shared-DarkMode.js
@@ -9,16 +9,25 @@ export default {
       },
       set (value) {
         this.$q.dark.set(value)
-        if (value) {
-          colors.setBrand('primary', 'var(--primaryForDarkMode)') // Only working in Quasar v1
-          colors.setBrand('textForPrimaryButton', 'var(--textForPrimaryButtonForDarkMode)') // Only working in Quasar v1
-          // colors.setCssVar('primary', '__ the value here __') // For when we move to Quasar v2
-        } else {
-          colors.setBrand('primary', 'var(--primaryForLightMode)') // Only working in Quasar v1
-          colors.setBrand('textForPrimaryButton', 'var(--textForPrimaryButtonForLightMode)') // Only working in Quasar v1
-        }
         localStorage.setItem('settings.darkMode', value)
+        this.setCssVariables(value)
       }
     }
-  } // computed
+  }, // computed
+  methods: {
+    setCssVariables (isDarkModeOn) {
+      if (isDarkModeOn) {
+        colors.setBrand('primary', 'var(--primaryForDarkMode)') // Only working in Quasar v1
+        colors.setBrand('textForPrimaryButton', 'var(--textForPrimaryButtonForDarkMode)') // Only working in Quasar v1
+        // colors.setCssVar('primary', '__ the value here __') // For when we move to Quasar v2
+      } else {
+        colors.setBrand('primary', 'var(--primaryForLightMode)') // Only working in Quasar v1
+        colors.setBrand('textForPrimaryButton', 'var(--textForPrimaryButtonForLightMode)') // Only working in Quasar v1
+      }
+    }
+  },
+  mounted () {
+    // Force to run it at loading time, so the CSS variables are always set correctly
+    this.setCssVariables(this.darkMode)
+  }
 }
