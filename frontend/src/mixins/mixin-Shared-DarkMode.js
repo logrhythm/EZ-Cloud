b6cb1a19c0aa1213cf028a4012708db148bba76a
TonyMasse <tony.masse@logrhythm.com>
2022-10-22 21:58:59 +0000

    Frontend - Add Keyboard shortcut to Dark mode mixin, for Dev mode

--- frontend/src/mixins/mixin-Shared-DarkMode.js
+++ frontend/src/mixins/mixin-Shared-DarkMode.js
@@ -24,10 +24,28 @@ export default {
         colors.setBrand('primary', 'var(--primaryForLightMode)') // Only working in Quasar v1
         colors.setBrand('textForPrimaryButton', 'var(--textForPrimaryButtonForLightMode)') // Only working in Quasar v1
       }
+    },
+    switchModeOnKeyDownEvent (event) {
+      // Only trigger if [CTRL]+[M] was pressed
+      // Ignore repeat triggers
+      if (!event.repeat && event.ctrlKey && event.key === 'm') {
+        this.darkMode = !this.darkMode
+      }
     }
   },
   mounted () {
     // Force to run it at loading time, so the CSS variables are always set correctly
     this.setCssVariables(this.darkMode)
+
+    // Allow for quick dark/day mode switch while in Dev mode, with [CTRL]+[M]
+    if (process.env.DEV) {
+      window.removeEventListener('keydown', this.switchModeOnKeyDownEvent)
+      window.addEventListener('keydown', this.switchModeOnKeyDownEvent)
+    }
+  },
+  beforeDestroy () {
+    if (process.env.DEV) {
+      window.removeEventListener('keydown', this.switchModeOnKeyDownEvent)
+    }
   }
 }
