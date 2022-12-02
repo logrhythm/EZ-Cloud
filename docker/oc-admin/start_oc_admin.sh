8eec9017773115b2ffbd96d0e2ea99cd8a155e09
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2022-12-02 18:26:39 +0100

    Backend - Docker - `start_oc_admin.sh` add Restart unless stopped

--- docker/oc-admin/start_oc_admin.sh
+++ docker/oc-admin/start_oc_admin.sh
@@ -4,8 +4,9 @@
 # Author:      Tony Massé
 # Create date: 2022-07-08
 # Modified on: 2022-07-26 - To add header and description
+# Modified on: 2022-12-02 - To add Restart unless stopped
 # Description: Pull and Start Production version of the OC-Admin container `oc-admin`.
 # =============================================
 
 echo "### RUN \`oc-admin\` CONTAINER..."
-docker run --detach --publish 8400:8400/tcp --network logrhythm --name oc-admin tonymasse/oc-admin
+docker run --detach --restart unless-stopped --publish 8400:8400/tcp --network logrhythm --name oc-admin tonymasse/oc-admin
