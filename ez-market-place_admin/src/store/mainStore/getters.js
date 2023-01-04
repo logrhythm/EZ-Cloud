0a4cd199a06854b5ac1c42849649c1a7acb0386e
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2023-01-04 17:27:12 +0000

    Remove useless access to `$auth`

--- ez-market-place_admin/src/store/mainStore/getters.js
+++ ez-market-place_admin/src/store/mainStore/getters.js
@@ -1,9 +1,5 @@
-import { createApp } from 'vue'
-
-const app = createApp()
-
 export function userIsLoggedIn (state) {
-  console.log('userIsLoggedIn', !!app.config.globalProperties.$auth.isAuthenticated, state.jwtToken.length, !!state.jwtToken.length)
+  console.log('userIsLoggedIn', state.jwtToken.length, !!state.jwtToken.length)
   // return !!app.config.globalProperties.$auth.isAuthenticated
   return !!state.jwtToken.length
 }
