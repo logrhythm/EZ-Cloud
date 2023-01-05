7ed2141dbe5040f6c71bdbe06099bc90c49b444d
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2023-01-05 19:29:19 +0000

    Fix: revert overide of `globalProperties`

--- ez-market-place_admin/src/boot/global-constants.js
+++ ez-market-place_admin/src/boot/global-constants.js
@@ -1,7 +1,7 @@
 import { boot } from 'quasar/wrappers'
 
 export default boot(({ app }) => {
-  app.config.globalProperties = {
+  app.config.globalProperties.globalConstants = {
     baseUrl: {
       website: '/EZ',
       api: '/EZ/API/v1'
@@ -9,16 +9,16 @@ export default boot(({ app }) => {
   }
 
   if (process.env.DEV) {
-    app.config.globalProperties.baseUrl.website = 'http://localhost:8442/EZ'
-    app.config.globalProperties.baseUrl.api = 'http://localhost:8442/API/v1'
+    app.config.globalProperties.globalConstants.baseUrl.website = 'http://localhost:8442/EZ'
+    app.config.globalProperties.globalConstants.baseUrl.api = 'http://localhost:8442/API/v1'
   }
 
   // Load from locally stored values
-  app.config.globalProperties.baseUrl.website = (localStorage.getItem('settings.ezMarketBackend.url.website') || app.config.globalProperties.baseUrl.website)
-  app.config.globalProperties.baseUrl.api = (localStorage.getItem('settings.ezMarketBackend.url.api') || app.config.globalProperties.baseUrl.api)
+  app.config.globalProperties.globalConstants.baseUrl.website = (localStorage.getItem('settings.ezMarketBackend.url.website') || app.config.globalProperties.globalConstants.baseUrl.website)
+  app.config.globalProperties.globalConstants.baseUrl.api = (localStorage.getItem('settings.ezMarketBackend.url.api') || app.config.globalProperties.globalConstants.baseUrl.api)
 
   if (process.env.DEV) {
     console.log('Global Constants:')
-    console.log(app.config.globalProperties)
+    console.log(app.config.globalProperties.globalConstants)
   }
 })
