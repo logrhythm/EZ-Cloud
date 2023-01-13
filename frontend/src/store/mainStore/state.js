6ff7e4ed6471d849e521d8881e92f38a074da84c
Tony Massé <50835088+TonyMasse@users.noreply.github.com>
2023-01-13 15:02:04 +0000

    Frontend - Use Marketplace AKA feature for accessing `docs.logrhythm.com`

--- frontend/src/store/mainStore/state.js
+++ frontend/src/store/mainStore/state.js
@@ -281,13 +281,17 @@ export default function () {
     userRoles: [], // Roles for EZ Users on EZ Server
     errorWikiUrlBase: ( // Base URL for the Documentation page with Error codes
       process.env.DEV
-        ? 'https://docs-staging.logrhythm.com/docs/OCbeats/logrhythm-open-collector/oc-admin/oc-admin-error-messages#OCAdminErrorMessages-'
-        : 'https://docs.logrhythm.com/docs/OCbeats/logrhythm-open-collector/oc-admin/oc-admin-error-messages#OCAdminErrorMessages-'
+        // ? 'https://docs-staging.logrhythm.com/docs/OCbeats/logrhythm-open-collector/oc-admin/oc-admin-error-messages#OCAdminErrorMessages-'
+        ? 'http://localhost:8443/aka/OCAdminErrorMessages-'
+        // : 'https://docs.logrhythm.com/docs/OCbeats/logrhythm-open-collector/oc-admin/oc-admin-error-messages#OCAdminErrorMessages-'
+        : 'https://journey.logrhythm.com/EZ/aka/OCAdminErrorMessages-'
     ),
     helpWikiUrlBase: ( // Base URL for the Help Documentation page
       process.env.DEV
-        ? 'https://docs-staging.logrhythm.com/docs/OCbeats/logrhythm-open-collector/oc-admin/navigating-and-using-oc-admin#NavigatingandUsingOCAdmin-'
-        : 'https://docs.logrhythm.com/docs/OCbeats/logrhythm-open-collector/oc-admin/navigating-and-using-oc-admin#NavigatingandUsingOCAdmin-'
+        // ? 'https://docs-staging.logrhythm.com/docs/OCbeats/logrhythm-open-collector/oc-admin/navigating-and-using-oc-admin#NavigatingandUsingOCAdmin-'
+        ? 'http://localhost:8443/aka/OCAdminHelp-'
+        // : 'https://docs.logrhythm.com/docs/OCbeats/logrhythm-open-collector/oc-admin/navigating-and-using-oc-admin#NavigatingandUsingOCAdmin-'
+        : 'https://journey.logrhythm.com/EZ/aka/OCAdminHelp-'
     ),
     deployment: {
       uid: null, // UID of the EZ Server
