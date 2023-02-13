89bf800fd3d7fb2d7d216f72c99bc0003e8ac1be
TonyMasse <tony.masse@logrhythm.com>
2023-02-13 21:53:07 +0000

    Backend - Socket - Docker Logs Tail - Fully functionning version

--- src/socket/controllers/containersLogs.js
+++ src/socket/controllers/containersLogs.js
@@ -1,3 +1,4 @@
+/* eslint-disable object-curly-newline */
 // Create SSH object
 const SSH = require('simple-ssh');
 
@@ -7,13 +8,13 @@ const { logToSystem } = require('../../shared/systemLogging');
 
 const containerLogsTails = [];
 
-function socketEmit(socket, stream = 'containerLogsTail.log', code = '', payload) {
-  if (socket.connected) {
-    socket.emit(stream, {
-      openCollectorUid: payload.openCollectorUid,
-      containerId: payload.containerId,
-      code,
-      payload
+function socketEmit(socket, payload = {}) {
+  if (socket && socket.connected) {
+    socket.emit(payload.stream || 'containerLogsTail.log', {
+      openCollectorUid: payload.openCollectorUid || '',
+      containerId: payload.containerId || '',
+      code: payload.code || '',
+      payload: payload.payload || ''
     });
   }
 }
@@ -40,72 +41,72 @@ async function containerLogsInit(socket, payload) {
       ) {
         containerLogsTails[`${socketUid}_${payload.openCollectorUid}`] = new SSH(configSsh);
 
-        socketEmit(socket, 'containerLogsTail.log', 'STDERR', '🚀 Container Stats Tail starting...');
-        socketEmit(socket, 'containerLogsTail.log', 'STDERR', '🎯 Attempting to connect to host...');
-        socketEmit(socket, 'containerLogsTail.log', 'STAGE', 'Container Stats Tail Started');
+        socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STDERR', payload: '🚀 OC ADMIN | Container Logs Tail starting...' });
+        socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STDERR', payload: '🎯 OC ADMIN | Attempting to connect to host...' });
+        socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STAGE', payload: 'Container Stats Tail Started' });
 
         containerLogsTails[`${socketUid}_${payload.openCollectorUid}`]
           // Check we are connected
           .exec('pwd', {
             exit() {
-              socketEmit(socket, 'containerLogsTail.log', 'STDERR', '📑 Tailing the realtime data...');
-              socketEmit(socket, 'containerLogsTail.log', 'STAGE', 'Connected to host');
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STDERR', payload: '📑 OC ADMIN | Tailing the realtime data...' });
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STAGE', payload: 'Connected to host' });
               return true;
             }
           })
           .exec(`docker logs --follow "${payload.containerId}"`, {
             err(stderr) {
               // console.log('STDERR:::' + stderr);
-              socketEmit(socket, 'containerLogsTail.log', 'STDERR', stderr);
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STDERR', payload: stderr });
             },
             exit(code) {
               // console.log('CODE:::' + code);
-              socketEmit(socket, 'containerLogsTail.log', 'EXIT', code);
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'EXIT', payload: code });
               // eslint-disable-next-line no-use-before-define
               setTimeout(containerLogsKillTail, 2500, socket, payload);
             },
             out(stdout) {
               // console.log('STDOUT:::' + stdout);
-              socketEmit(socket, 'containerLogsTail.log', 'STAGE', 'Receiving Real Time Data');
-              socketEmit(socket, 'containerLogsTail.log', 'STDOUT', stdout);
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STAGE', payload: 'Receiving Real Time Data' });
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STDOUT', payload: stdout });
             }
           })
           .on('end', (err) => {
             // console.log('END:::' + err);
-            socketEmit(socket, 'containerLogsTail.log', 'STAGE', 'Container Stats Tail Ended');
-            socketEmit(socket, 'containerLogsTail.log', 'END', err);
-            socketEmit(socket, 'containerLogsTail.log', 'STAGE', 'Stopped');
+            socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STAGE', payload: 'Container Stats Tail Ended' });
+            socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'END', payload: err });
+            socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STAGE', payload: 'Stopped' });
             // eslint-disable-next-line no-use-before-define
             setTimeout(containerLogsKillTail, 1000, socket, payload);
           })
           .start({
             failure() {
               // console.log('FAILURE:::' + err);
-              socketEmit(socket, 'containerLogsTail.log', 'FAILURE');
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'FAILURE' });
             }
           });
       } else {
-        socketEmit(socket, 'containerLogsTail.log', 'ERROR', '❌ Container Stats Tail failed to start due to missing OpenCollector host and/or port information for the SSH connection.');
-        socketEmit(socket, 'containerLogsTail.log', 'EXIT', 1);
-        socketEmit(socket, 'containerLogsTail.log', 'END', 'Container Stats Tail failed to start.');
-        socketEmit(socket, 'containerLogsTail.log', 'STAGE', 'Stopped');
-        logToSystem('Warning', 'containerLogsInit - Container Stats Tail failed to start due to missing OpenCollector host and/or port information for the SSH connection.');
+        socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'ERROR', payload: '❌ Container Logs Tail failed to start due to missing OpenCollector host and/or port information for the SSH connection.' });
+        socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'EXIT', payload: 1 });
+        socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'END', payload: 'Container Logs Tail failed to start.' });
+        socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STAGE', payload: 'Stopped' });
+        logToSystem('Warning', 'containerLogsInit - Container Logs Tail failed to start due to missing OpenCollector host and/or port information for the SSH connection.');
       }
     } else {
       // The openCollectorUid does already exist
-      socketEmit(socket, 'containerLogsTail.log', 'ERROR', '❌ Container Stats Tail failed to start due to another Container Stats Tail with the same openCollectorUid already exists.');
-      socketEmit(socket, 'containerLogsTail.log', 'EXIT', 1);
-      socketEmit(socket, 'containerLogsTail.log', 'END', 'Container Stats Tail failed to start.');
-      socketEmit(socket, 'containerLogsTail.log', 'STAGE', 'Stopped');
-      logToSystem('Warning', 'containerLogsInit - Container Stats Tail failed to start due to another Container Stats Tail with the same openCollectorUid already exists.');
+      socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'ERROR', payload: '❌ Container Logs Tail failed to start due to another Container Logs Tail with the same openCollectorUid already exists.' });
+      socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'EXIT', payload: 1 });
+      socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'END', payload: 'Container Logs Tail failed to start.' });
+      socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STAGE', payload: 'Stopped' });
+      logToSystem('Warning', 'containerLogsInit - Container Logs Tail failed to start due to another Container Logs Tail with the same openCollectorUid already exists.');
     }
   } else {
     // Required Payload absent or incomplete
-    socketEmit(socket, 'containerLogsTail.log', 'ERROR', '❌ Container Stats Tail failed to start due to incomplete Payload in request.');
-    socketEmit(socket, 'containerLogsTail.log', 'EXIT', 1);
-    socketEmit(socket, 'containerLogsTail.log', 'END', 'Container Stats Tail failed to start.');
-    socketEmit(socket, 'containerLogsTail.log', 'STAGE', 'Stopped');
-    logToSystem('Error', 'containerLogsInit - Container Stats Tail failed to start due to incomplete Payload in request.');
+    socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'ERROR', payload: '❌ Container Logs Tail failed to start due to incomplete Payload in request.' });
+    socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'EXIT', payload: 1 });
+    socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'END', payload: 'Container Logs Tail failed to start.' });
+    socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.log', code: 'STAGE', payload: 'Stopped' });
+    logToSystem('Error', 'containerLogsInit - Container Logs Tail failed to start due to incomplete Payload in request.');
   }
 } // containerLogsInit
 
@@ -143,49 +144,71 @@ async function containerLogsKillTail(socket, payload) {
         containerLogsTails[`${socketUid}_${payload.openCollectorUid}`] = new SSH(configSsh);
 
         containerLogsTails[`${socketUid}_${payload.openCollectorUid}`]
-          .exec(`ps auxwww | grep -v "grep" | grep "docker logs --follow" | grep "${payload.containerId}" -q && exit 42;`, {
+          .exec(`ps xwww -o pid,args | grep -v "grep" | grep "docker logs --follow" | grep "${payload.containerId}" -q && exit 42;`, {
             exit(code) {
               if (code === 42) {
                 // If Docker Stats is still running for this OpenCollector,
                 // go ahead and kill it.
-                socketEmit(socket, 'containerLogsTail.kill', 'STDOUT', 'Docker Logs is still running for this OpenCollector. As expected. Now going for the kill.');
+                socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'STDOUT', payload: 'Docker Logs is still running for this OpenCollector. As expected. Now going for the kill.' });
                 return true;
               }
-              socketEmit(socket, 'containerLogsTail.kill', 'ERROR', 'Docker Logs does\'t seem to be running for this OpenCollector. Doing nothing.');
-              socketEmit(socket, 'containerLogsTail.kill', 'EXIT', code);
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'ERROR', payload: 'Docker Logs does\'t seem to be running for this OpenCollector. Doing nothing.' });
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'EXIT', payload: code });
               return false;
             }
           })
-          .exec(`kill $(ps auxwwww | grep -v "grep" | grep "docker logs --follow" | grep "${payload.containerId}" | grep -o "^\\s*\\d\\+") && echo -e "Docker Logs process terminated.";`, {
+          .exec(`ps xwww -o pid,args | grep -v "grep" | grep "docker logs --follow" | grep "${payload.containerId}" | grep -o "^[0-9]\\+"`, {
             err(stderr) {
               // console.log('STDERR:::' + stderr);
-              socketEmit(socket, 'containerLogsTail.kill', 'STDERR', stderr);
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'STDERR', payload: stderr });
             },
             exit(code) {
               // console.log('CODE:::' + code);
               if (code !== 0) {
-                socketEmit(socket, 'containerLogsTail.kill', 'ERROR', 'Something didn\'t go according to plan while trying to terminate Docker Logs\' Process.');
+                socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'ERROR', payload: 'Failed to grab Docker Logs\' Process ID.' });
                 return false;
               }
-              socketEmit(socket, 'containerLogsTail.kill', 'EXIT', code);
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'EXIT', payload: code });
               return true;
             },
             out(stdout) {
               // console.log('STDOUT:::' + stdout);
-              socketEmit(socket, 'containerLogsTail.kill', 'STDOUT', stdout);
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'STDOUT', payload: `Docker Logs' Process ID(s): ${stdout}` });
+            }
+          })
+          .exec(`kill $(ps xwww -o pid,args | grep -v "grep" | grep "docker logs --follow" | grep "${payload.containerId}" | grep -o "^[0-9]\\+") && echo -e "Docker Logs process terminated.";`, {
+            err(stderr) {
+              // console.log('STDERR:::' + stderr);
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'STDERR', payload: stderr });
+            },
+            exit(code) {
+              // console.log('CODE:::' + code);
+              if (code !== 0) {
+                socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'ERROR', payload: 'Something didn\'t go according to plan while trying to terminate Docker Logs\' Process.' });
+                return false;
+              }
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'EXIT', payload: code });
+              return true;
+            },
+            out(stdout) {
+              // console.log('STDOUT:::' + stdout);
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'STDOUT', payload: stdout });
             }
           })
           .on('end', (err) => {
             // console.log('END:::' + err);
-            socketEmit(socket, 'containerLogsTail.kill', 'END', err);
+            socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'END', payload: err });
           })
           .start({
             failure() {
               // console.log('FAILURE:::' + err);
-              socketEmit(socket, 'containerLogsTail.kill', 'FAILURE');
+              socketEmit(socket, { containerId: payload.containerId, openCollectorUid: payload.openCollectorUid, stream: 'containerLogsTail.kill', code: 'FAILURE' });
             }
           });
       }
+    } else {
+      // Required Payload absent or incomplete
+      logToSystem('Error', 'containerLogsKillTail - Container Logs Tail could not be killed dues to incomplete Payload in request.');
     }
   } catch (err) {
     //
